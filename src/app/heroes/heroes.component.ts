6f5e543e876ab0d3459fd7f71530bc53129d31b9
AlexJamisone <business.homeit@gmail.com>
2022-08-30 14:13:02 +0300

    add hero data, and use rxJS Observer for fetch fake data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -7,9 +9,22 @@ import { HEROES } from '../mock-heroes';
 	styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-	heroes = HEROES;
-
-	constructor() {}
-
-	ngOnInit(): void {}
+	
+	selectedHero?: Hero;
+	heroes: Hero[] = [];
+	
+	
+	constructor(private heroService: HeroService, private messageService: MessageService) {}
+	
+	ngOnInit(): void {
+		this.getHeroes()
+	}
+	onSelect(heroes: Hero): void {
+		this.selectedHero = heroes;
+		this.messageService.add(`HeroesComponent: Selected hero id${heroes.id}`)
+	}
+	getHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes)
+	}
 }
