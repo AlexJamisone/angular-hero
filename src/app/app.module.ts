1e75666c5f1dd1ba9cd9e98c83c01fa30ca40ba1
AlexJamisone <business.homeit@gmail.com>
2022-08-30 16:38:10 +0300

    Start imulate realData base with in memory amngula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,31 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroesComponent,
+		HeroDetailComponent,
+		MessageComponent,
+		DashboardComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+			dataEncapsulation: false,
+		}),
+	],
+
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -9,22 +8,18 @@ import { MessageService } from '../message.service';
 	styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-	
-	selectedHero?: Hero;
 	heroes: Hero[] = [];
-	
-	
-	constructor(private heroService: HeroService, private messageService: MessageService) {}
-	
+
+	constructor(
+		private heroService: HeroService,
+	) {}
+
 	ngOnInit(): void {
-		this.getHeroes()
-	}
-	onSelect(heroes: Hero): void {
-		this.selectedHero = heroes;
-		this.messageService.add(`HeroesComponent: Selected hero id${heroes.id}`)
+		this.getHeroes();
 	}
 	getHeroes(): void {
-		this.heroService.getHeroes()
-			.subscribe(heroes => this.heroes = heroes)
+		this.heroService
+			.getHeroes()
+			.subscribe((heroes) => (this.heroes = heroes));
 	}
 }
